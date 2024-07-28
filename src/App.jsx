3832be774483dd96d3938aa1b9078e65a3778d08
root <root@Laptop-Ehsan>
2024-07-28 11:50:01 +0330

    Put inputs inside general component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { useState } from "react";
 import "./styles/App.css";
-import Info from "./components/Info";
 import General from "./components/General";
 import Education from "./components/Education";
 
 function App() {
   return (
     <div className="page-container">
-      <General
-        inputs={[
-          <Info infoName={"Name"} type={"text"} key={0} />,
-          <Info infoName={"Email"} type={"email"} key={1} />,
-          <Info infoName={"Phone Number"} type={"tel"} key={2} />,
-        ]}
-      />
+      <General />
       <Education />
     </div>
   );
