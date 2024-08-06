3d879b6ca8194953a9affa0eff499256705e4fb5
root <root@Laptop-Ehsan>
2024-08-06 16:36:50 +0330

    Change some styles

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -181,7 +181,24 @@ export default function Education() {
       <div className="section-top">
         <h2>Education</h2>
         <button className="add-button" onClick={handleOpenClose}>
-          Add <span>+</span>
+          Add{" "}
+          <span>
+            <svg
+              width="800px"
+              height="800px"
+              viewBox="0 0 24 24"
+              fill="white"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6 12H18M12 6V18"
+                stroke="#000000"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
         </button>
       </div>
       <div className="education-container">
